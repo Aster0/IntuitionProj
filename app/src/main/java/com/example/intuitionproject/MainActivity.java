8d7c2dc04c54cd1b111dfe9f877ee58ffeebe9c4
joey <jo-_-ey@hotmail.com>
2021-02-27 14:00:28 +0800

    Navigation between register and login page

--- app/src/main/java/com/example/intuitionproject/MainActivity.java
+++ app/src/main/java/com/example/intuitionproject/MainActivity.java
@@ -2,25 +2,37 @@ package com.example.intuitionproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.example.intuitionproject.databinding.ActivityMainBinding;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
         Map<String, String> test = new HashMap<>();
 
         test.put("test", "testtt");
 
         FirebaseFirestore.getInstance().collection("testing").document("test").set(test);
-        //test commit - Jordy
+        binding.btnSignUpPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/intuitionproject/SignUpActivity.java
+++ app/src/main/java/com/example/intuitionproject/SignUpActivity.java
@@ -2,13 +2,27 @@ package com.example.intuitionproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.intuitionproject.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
+    private ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.btnLoginPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }

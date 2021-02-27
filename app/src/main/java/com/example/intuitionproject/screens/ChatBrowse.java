78c4aec230e0c020b529b3199308bc776583f8e0
Astero <asterodevs@gmail.com>
2021-02-27 13:16:59 +0800

    added screens folder

---
+++ app/src/main/java/com/example/intuitionproject/screens/ChatBrowse.java
@@ -0,0 +1,16 @@
+package com.example.intuitionproject.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.intuitionproject.R;
+
+public class ChatBrowse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_browse);
+    }
+}

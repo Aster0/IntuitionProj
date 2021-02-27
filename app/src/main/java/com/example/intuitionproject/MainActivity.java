97ac43fe43545bad8959bb2091766a86b3ccbcb1
Jordan Khong <jordynwinnie@gmail.com>
2021-02-27 17:43:25 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/intuitionproject/MainActivity.java
+++ app/src/main/java/com/example/intuitionproject/MainActivity.java
@@ -1,28 +1,21 @@
 package com.example.intuitionproject;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.util.Patterns;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import com.example.intuitionproject.databinding.ActivityMainBinding;
-import com.example.intuitionproject.screens.ChatBrowse;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -62,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
                 login(email, passsword);
             }
         });
-        
+
     }
 
     private void login(String email, String password) {

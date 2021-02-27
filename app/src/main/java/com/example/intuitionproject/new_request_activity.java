a71272f34f4ef17cd9dbd10298770eed3b3b68c7
Jordan Khong <jordynwinnie@gmail.com>
2021-02-27 13:40:13 +0800

    Added Request UI

---
+++ app/src/main/java/com/example/intuitionproject/new_request_activity.java
@@ -0,0 +1,17 @@
+package com.example.intuitionproject;
+
+import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.View;
+
+public class new_request_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_request_activity);
+    }
+}

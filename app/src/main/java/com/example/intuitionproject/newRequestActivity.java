64f96114e3b6ebcec7076c2b2eed2dbb5e33b2ab
Jordan Khong <jordynwinnie@gmail.com>
2021-02-27 15:10:09 +0800

    Added Image Picker

--- app/src/main/java/com/example/intuitionproject/newRequestActivity.java
+++ app/src/main/java/com/example/intuitionproject/newRequestActivity.java
@@ -1,13 +1,21 @@
 package com.example.intuitionproject;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class newRequestActivity extends AppCompatActivity {
+import com.vansuita.pickimage.bean.PickResult;
+import com.vansuita.pickimage.bundle.PickSetup;
+import com.vansuita.pickimage.dialog.PickImageDialog;
+import com.vansuita.pickimage.listeners.IPickResult;
+
+public class newRequestActivity extends AppCompatActivity implements IPickResult {
 
     public String[] SingaporeTowns = { "None", "Ang Mo Kio", "Bedok", "Bishan", "Bukit Batok", "Red Hill",
             "Bukit Panjang", "Choa Chu Kang", "Clementi", "Geylang", "Hougang", "Jurong East", "Jurong West",
@@ -18,6 +26,7 @@ public class newRequestActivity extends AppCompatActivity {
     Spinner meetupRegion;
     Spinner destinationRegion;
     EditText deliveryPrice;
+    ImageButton captureImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,5 +43,38 @@ public class newRequestActivity extends AppCompatActivity {
 
         meetupRegion.setAdapter(new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, SingaporeTowns));
         destinationRegion.setAdapter(new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, SingaporeTowns));
+        captureImage = findViewById(R.id.item_image);
+
+        captureImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PickSetup setup = new PickSetup().setTitle("Upload Image").setSystemDialog(true).setProgressText("Hello");
+
+                PickImageDialog.build(setup).show(newRequestActivity.this);
+            }
+        });
+    }
+
+
+    @Override
+    public void onPickResult(PickResult r) {
+        if (r.getError() == null) {
+            //If you want the Uri.
+            //Mandatory to refresh image from Uri.
+            //getImageView().setImageURI(null);
+
+            //Setting the real returned image.
+            //getImageView().setImageURI(r.getUri());
+
+            //If you want the Bitmap.
+           captureImage.setImageBitmap(r.getBitmap());
+
+            //Image path
+            //r.getPath();
+        } else {
+            //Handle possible errors
+            //TODO: do what you have to do with r.getError();
+            Toast.makeText(this, r.getError().getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 }
